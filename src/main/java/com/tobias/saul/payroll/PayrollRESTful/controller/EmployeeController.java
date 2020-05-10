c8ec8a92bba87ecafea3b86a7e312c9e737e84be
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-10 13:12:21 -0500

    added restful response to getEmployees

--- src/main/java/com/tobias/saul/payroll/PayrollRESTful/controller/EmployeeController.java
+++ src/main/java/com/tobias/saul/payroll/PayrollRESTful/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.tobias.saul.payroll.PayrollRESTful.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import org.springframework.hateoas.CollectionModel;
@@ -22,20 +23,28 @@ import com.tobias.saul.payroll.PayrollRESTful.service.EmployeeService;
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
-	
+
 	@Autowired
 	private EmployeeService employeeService;
-	
+
 	@GetMapping("/employees")
-	public List<Employee> getEmployees() {
-		return employeeService.findAllEmployees();
+	public CollectionModel<EntityModel<Employee>> getEmployees() {
+
+		List<EntityModel<Employee>> employees = employeeService.findAllEmployees().stream()
+				.map(employee -> new EntityModel<Employee>(employee,
+						linkTo(methodOn(EmployeeController.class).getEmployee(employee.getId())).withSelfRel(),
+						linkTo(methodOn(EmployeeController.class).getEmployees()).withRel("employees")))
+				.collect(Collectors.toList());
+
+		return new CollectionModel<>(employees,
+				linkTo(methodOn(EmployeeController.class).getEmployees()).withSelfRel());
 	}
-	
+
 	@PostMapping("/employees")
 	public Employee createEmployee(@RequestBody Employee employee) {
 		return employeeService.createEmployee(employee);
 	}
-	
+
 	@GetMapping("/employees/{employeeId}")
 	public EntityModel<Employee> getEmployee(@PathVariable("employeeId") Long employeeId) {
 		Employee employee = employeeService.findEmployee(employeeId);
@@ -43,13 +52,12 @@ public class EmployeeController {
 				linkTo(methodOn(EmployeeController.class).getEmployee(employeeId)).withSelfRel(),
 				linkTo(methodOn(EmployeeController.class).getEmployees()).withRel("employees"));
 	}
-	
+
 	@PutMapping("/employees/{employeeId}")
-	public Employee updateEmployee(@RequestBody Employee employee, 
-			@PathVariable("employeeId") Long employeeId) {
+	public Employee updateEmployee(@RequestBody Employee employee, @PathVariable("employeeId") Long employeeId) {
 		return employeeService.updateEmployee(employee, employeeId);
 	}
-	
+
 	@DeleteMapping("/employees/{employeeId}")
 	public void deleteEmployee(@PathVariable("employeeId") Long employeeId) {
 		employeeService.deleteEmployee(employeeId);
