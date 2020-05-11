d6b175301e148bbbef7b2cb35b98af3c5e8fb72b
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-11 00:05:37 -0500

    added rest responses to operations

--- src/main/java/com/tobias/saul/payroll/PayrollRESTful/assembler/OrderModelAssembler.java
+++ src/main/java/com/tobias/saul/payroll/PayrollRESTful/assembler/OrderModelAssembler.java
@@ -4,15 +4,20 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
 
+import com.tobias.saul.payroll.PayrollRESTful.controller.OrderController;
 import com.tobias.saul.payroll.PayrollRESTful.model.Order;
 
+@Component
 public class OrderModelAssembler implements RepresentationModelAssembler<Order, EntityModel<Order>>{
 
 	@Override
 	public EntityModel<Order> toModel(Order order) {
 		// TODO Auto-generated method stub
-		return null;
+		return new EntityModel<Order>(order, 
+				linkTo(methodOn(OrderController.class).getOrder(order.getId())).withSelfRel(),
+				linkTo(methodOn(OrderController.class).getOrders()).withRel("orders"));
 	}
 
 }
---
+++ src/main/java/com/tobias/saul/payroll/PayrollRESTful/controller/OrderController.java
@@ -0,0 +1,89 @@
+package com.tobias.saul.payroll.PayrollRESTful.controller;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tobias.saul.payroll.PayrollRESTful.assembler.OrderModelAssembler;
+import com.tobias.saul.payroll.PayrollRESTful.model.Order;
+import com.tobias.saul.payroll.PayrollRESTful.model.OrderStatus;
+import com.tobias.saul.payroll.PayrollRESTful.service.OrderService;
+
+@RestController
+@RequestMapping("/api/v1")
+public class OrderController {
+
+	private final OrderService orderService;
+	private final OrderModelAssembler assembler;
+
+	@Autowired
+	public OrderController(OrderService orderService, OrderModelAssembler assembler) {
+		this.orderService = orderService;
+		this.assembler = assembler;
+	}
+
+	@GetMapping("/orders/{orderId}")
+	public EntityModel<Order> getOrder(@PathVariable("orderId") Long orderId) {
+		Order order = orderService.findOrder(orderId);
+		
+		return assembler.toModel(order);
+	}
+
+	@GetMapping("/orders")
+	public CollectionModel<EntityModel<Order>> getOrders() {
+		
+		List<EntityModel<Order>> orders = orderService.findAllOrders().stream()
+				.map(assembler::toModel)
+				.collect(Collectors.toList());
+		
+		return new CollectionModel<>(orders,
+				linkTo(methodOn(OrderController.class).getOrders()).withSelfRel());
+	}
+	
+	@PostMapping("/orders")
+	public ResponseEntity<?> createOrder(@RequestBody Order order) {
+		
+		order.setOrderStatus(OrderStatus.IN_PROGRESS);
+
+		EntityModel<Order> orderModel = assembler
+				.toModel(orderService.createOrder(order));
+
+		return ResponseEntity.created(orderModel.getRequiredLink(IanaLinkRelations.SELF)
+				.toUri()).body(orderModel);
+	}
+	
+	@PutMapping("/orders/{orderId}")
+	public ResponseEntity<?> updateOrder(@RequestBody Order newOrder, @PathVariable("orderId") Long orderId) {
+		
+		EntityModel<Order> orderModel = assembler
+				.toModel(orderService.updateOrder(newOrder, orderId));
+		
+		return ResponseEntity.created(orderModel.getRequiredLink(IanaLinkRelations.SELF)
+				.toUri()).body(orderModel);
+	}
+	
+	@DeleteMapping("/orders/{orderId}")
+	public ResponseEntity<?> deleteOrder(@PathVariable("orderId") Long orderId) {
+		
+		orderService.deleteOrder(orderId);
+		
+		return ResponseEntity.noContent().build();
+	}
+
+}
